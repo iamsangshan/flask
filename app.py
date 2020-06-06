df45a07a1d95299bfcf6706364edd92255f74895
iamsangshan <58853757+iamsangshan@users.noreply.github.com>
2020-06-06 15:20:58 +0530

    Initial commit of Flask 3 hours tutorial

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+#give a name to the app
+app = Flask(__name__)
+
+#add a URL to the app, currently localhost
+@app.route('/')
+def hello() :
+    return "Hello World!"
+# if the app is running in shell command,
+# good practise to turn the debug on (dev mode)
+if __name__ == "__main__":
+    app.run(debug=True)
