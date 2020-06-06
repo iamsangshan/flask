ba82c26a473b3c4638e535dfff2c11878e909f34
iamsangshan <58853757+iamsangshan@users.noreply.github.com>
2020-06-06 17:24:57 +0530

    passing arguments from py to html

--- app.py
+++ app.py
@@ -3,13 +3,31 @@ from flask import Flask, render_template
 #give a name to the app
 app = Flask(__name__)
 
+# Our dummy data
+stories = [
+    {
+        'title' : 'Story 1',
+        'content' : 'Once upon a time...'
+    },
+    {
+        'title' : 'Story 2',
+        'content' : 'And they lived happily everafter...'
+    }
+]
+
 #add a URL to the app, currently localhost#
 #@app.route('/')
 
 #pass an argument to the function and also add additional URL
-@app.route('/home/<string:person>')
-def hello(person) :
+@app.route('/home/')
+def hello() :
     return render_template('index.html')
+
+#pass the stories as a variable to the posts.html
+@app.route('/home/posts')
+def posts():
+    return render_template('posts.html', allstories=stories)
+
 # if the app is running in shell command,
 # good practise to turn the debug on (dev mode)
 if __name__ == "__main__":
