83fe04a2ac7b60e35b796ffbf3c3b24866184ead
iamsangshan <58853757+iamsangshan@users.noreply.github.com>
2020-06-06 16:44:10 +0530

    arguments

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask
 #give a name to the app
 app = Flask(__name__)
 
-#add a URL to the app, currently localhost
-@app.route('/')
-def hello() :
-    return "Hello World. This is a FLask App!"
+#add a URL to the app, currently localhost#
+#@app.route('/')
+
+#pass an argument to the function and also add additional URL
+@app.route('/home/<string:person>')
+def hello(person) :
+    return "Hello, Welcome " + person + " !"
 # if the app is running in shell command,
 # good practise to turn the debug on (dev mode)
 if __name__ == "__main__":
     app.run(debug=True)
+
 
