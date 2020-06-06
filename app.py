7c416da120b09fadb9fc0f8429c40dac22d53518
iamsangshan <58853757+iamsangshan@users.noreply.github.com>
2020-06-06 15:30:12 +0530

    Second commit

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 #add a URL to the app, currently localhost
 @app.route('/')
 def hello() :
-    return "Hello World!"
+    return "Hello World. This is a FLask App!"
 # if the app is running in shell command,
 # good practise to turn the debug on (dev mode)
 if __name__ == "__main__":
     app.run(debug=True)
+
