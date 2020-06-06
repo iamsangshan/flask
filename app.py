dbdcf7e1dc1475b89ab29b4343c2ec691eced2dd
iamsangshan <58853757+iamsangshan@users.noreply.github.com>
2020-06-06 16:55:53 +0530

    templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #give a name to the app
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #pass an argument to the function and also add additional URL
 @app.route('/home/<string:person>')
 def hello(person) :
-    return "Hello, Welcome " + person + " !"
+    return render_template('index.html')
 # if the app is running in shell command,
 # good practise to turn the debug on (dev mode)
 if __name__ == "__main__":
